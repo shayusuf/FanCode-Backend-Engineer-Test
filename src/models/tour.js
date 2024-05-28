c61713e6ce91483d6962c6385b235493bf2a0ed1
shayusuf <yusufcricket200@gmail.com>
2024-05-28 15:24:46 +0530

    Problem1: modified endpoint

--- src/models/tour.js
+++ src/models/tour.js
@@ -7,7 +7,27 @@ const getAllTours = async () => {
 }
 
 const getMatchesByTourName = async params => {
-    const statement = 'select * from matches left join tours on matches.tourId = tours.id where tours.name = ?';
+    /**
+     * Problem 1: 
+     * 
+     * Primary key `id` and foreign key `tourId` of `matches` table and 
+     * primary key `id` of `tours` table are indexed by default by a SQL engine,
+     * so joins will be fast when these columns are used for join criterion. 
+     * 
+     * `name` column of `tours` table is not indexed; to quickly fetch the `id` of a tour
+     * given the tour name we may have to index `name` of a tour.
+     * 
+     * this way we can fetch tour `id` from `name` in O(1) time and with tour `id` 
+     * whose corresponding value in `matches` table is `tourId` which is also indexed so we can
+     * fetch matches in O(1) time. This can be achieved through a sub-query to get tour id from 
+     * `tours` table and then querying the matched table.
+     * 
+     * As per my understanding the systems seems to be a Read heavy than Write,
+     * so event though writes (update/add/delete) to `tours` table might increase write latency
+     * due to increased index management but it won't affect the user experience to a big extent.
+     * 
+     */
+    const statement = 'select * from matches where tourId = (select id from tours where name = ?)';
     const parameters = [ params.name ];
     return await mysql.query(statement, parameters);
 }
