4906e11723377b1bc266b5308b3b306dae89611f
shayusuf <yusufcricket200@gmail.com>
2024-05-28 15:31:15 +0530

    Problem2: modified endpoint

--- src/controllers/sport.js
+++ src/controllers/sport.js
@@ -1,17 +1,23 @@
 const Sport = require('../models/sport');
 
+// Problem2
 const getAllSportsToursAndMatches = async () => {
     const matches = await Sport.getAllSportsToursAndMatches();
     const res = {};
     matches.forEach(match => {
-        const { sportName, tourName, matchName } = match;
+        const { sportName, tourName, matchName, matchId, matchStartTime, matchFormat } = match;
         if (!res[sportName]) {
             res[sportName] = {};
         }
         if (!res[sportName][tourName]) {
             res[sportName][tourName] = [];
         }
-        res[sportName][tourName].push(matchName);
+        res[sportName][tourName].push({
+            'id': matchId,
+            'name': matchName,
+            'startTime': matchStartTime,
+            'format': matchFormat 
+        });
     });
     return res;
 }
